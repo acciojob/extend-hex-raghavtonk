e8bd9d914e69f0dcd4b24adcc1eeeff58649ce43
Raghav Kumar Tonk <86098245+raghavtonk@users.noreply.github.com>
2024-02-05 22:09:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 const extendHex = (shortHex) => {
   // write your code here
+	
+	let fullHex = "#"
+		
+	if(shortHex.charAt(0) != "#")
+	{
+		  
+		for(i = 0; i < shortHex.length;i++){
+			fullHex += shortHex.charAt(i)+ shortHex.charAt(i);
+		}
+	}
+	else{
+		for(i = 1; i < shortHex.length;i++){
+			fullHex += shortHex.charAt(i)+ shortHex.charAt(i);
+		}
+	}
+	return fullHex 
+	 
 };
 
 // Do not change the code below.
 const shortHex = prompt("Enter Short Hex.");
 alert(extendHex(shortHex));
+ 
